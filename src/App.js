6af30cfefa5ad78d3ed4bc0a668d7c7bc3e43ca4
Gwen <gwenael.leroutier@gmail.com>
2018-09-05 01:34:26 +0200

    purposal for a new fancy name #3

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ CreateTask = (event) => {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my amazing To do app</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
